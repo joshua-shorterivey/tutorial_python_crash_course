1f1c74dd7125052c016423dc244ecb60aa024b26
joshua-shorterivey <joshua.shorterivey@gmail.com>
2022-12-21 14:01:25 -0600

    added initial modules from chapter 2

---
+++ ex-2-1_simple_messages.py
@@ -0,0 +1,9 @@
+# Write a separate program to accomplish each of these exercises. Save each program with a filename that follows standard Python conventions, using lowercase letters and underscores, such as simple_message.py and simple _messages.py.
+### ------ ###
+# 2-1. Simple Message: Assign a message to a variable, and then print that message.
+first_variable = 'This is the first variable'
+print(first_variable)
+
+# 2-2. Simple Messages: Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+first_variable = 'This is the manipulated version of the first variable'
+print(first_variable)
