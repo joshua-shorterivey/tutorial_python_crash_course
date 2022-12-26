2698f94052a0feb155297db2c2d245fd7359d073
joshua-shorterivey <joshua.shorterivey@gmail.com>
2022-12-26 14:45:19 -0600

    added chapter 8

---
+++ ch8_greeter.py
@@ -0,0 +1,14 @@
+#defining a function
+
+def greet_user():
+    """ Display a simple greeting."""
+    print('Hello!')
+
+greet_user()
+
+# passing information to a function
+def greet_user_b(username):
+    """ Display a simple greeting."""
+    print(f'Hello, {username} ')
+
+greet_user_b('jesse')
